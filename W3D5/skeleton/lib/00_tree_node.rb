e8a2267fa8b6fb1707a9627eeb972a18c54924d2
IanDMcGrath <ian92415@gmail.com>
2021-07-23 13:25:37 -0700

    added initialize and parent methods

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,20 @@
 class PolyTreeNode
-
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+    
+    def parent=(parent)
+        # if new parent, remove self from parent's children array
+        @parent = parent
+        
+        if @parent != nil && !@parent.children.include?(self)
+            @parent.children << self
+        end
+    end
+    
+    
+    attr_reader :value, :parent, :children
 end
+
