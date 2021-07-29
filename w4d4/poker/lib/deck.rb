1b30512a4277dbed60fb1ef4eca5dea270c2c035
JoeJoegb922 <josephlam922@gmail.com>
2021-07-29 16:18:37 -0700

    finished card spec and started deck

---
+++ w4d4/poker/lib/deck.rb
@@ -0,0 +1,11 @@
+
+SUITS = [:hearts, :diamonds, :clubs, :spades]
+VALUES = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
+
+class Deck
+   
+    def initialize 
+        @deck = []
+        #stack deck 
+    end
+end
--- w4d4/poker/spec/deck_spec.rb
+++ w4d4/poker/spec/deck_spec.rb
@@ -1,1 +1,24 @@
 require "deck"
+
+describe "deck" do 
+    deck = Deck.new
+    it "should define class constant available SUITS" do
+        expect(SUITS.is_a?(Array)).to eq true
+    end
+
+    it "should define class constant available VALUES" do
+        expect(VALUES.is_a?(Array)).to eq true
+    end
+
+    describe "initialize" do 
+        it "should set deck equal to build_deck" do 
+
+        end
+
+        it "should call ::build_deck" do 
+
+        end
+
+        it 
+    end
+end
