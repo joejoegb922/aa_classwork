9d95ed78e4d56d7eed42e2b4cb69d8d0f7ba1d4c
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-07-29 12:14:32 -0700

    added two_sum method

--- w4d4/lib/tdd.rb
+++ w4d4/lib/tdd.rb
@@ -1,6 +1,19 @@
 
-def uniq(array)
+def uniq(array) 
     new_arr = []
     array.each { |ele| new_arr << ele if !new_arr.include?(ele) }
     new_arr
+end
+
+class Array
+
+  def two_sum
+    sums = []
+    (0...self.length).each do |i|
+      (i+1...self.length).each do |j|
+        sums << [i,j] if self[i] + self[j] == 0
+      end
+    end
+    sums
+  end
 end
--- w4d4/spec/tdd_spec.rb
+++ w4d4/spec/tdd_spec.rb
@@ -12,8 +12,19 @@ describe "TDD_1" do
 end
 
 describe "Array" do
-    describe "#two_sum" do 
-        [-1, 0, 2, -2, 1].two_sum
+    describe "#two_sum" do         #[-1, 0, 2, -2, 1].two_sum 
+      it "should accept an array without raising error" do 
+        [1,0,0,-1,0,4].two_sum
+      end
+      it "should return an array of index's that sum to 0" do
+        expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+      end
+      it "should not return ann array of the same index" do 
+        expect([-1, 0, 2, -2, 1].two_sum).not_to eq([[0, 4],[1,1],[2, 3]])
+      end
+      it "should return indexes in order" do 
+        expect([-1, 0, 2, -2, 1].two_sum).not_to eq([[2,3],[0,4]])
+      end
     end
 end
 
