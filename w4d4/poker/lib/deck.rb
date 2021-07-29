31cca12587637c96b69faf0f6c1835c9272b0768
Maisie Bruno-Tyne <maisiejillb@gmail.com>
2021-07-29 16:58:44 -0700

    finished deck and card

--- w4d4/poker/lib/deck.rb
+++ w4d4/poker/lib/deck.rb
@@ -1,11 +1,24 @@
 
+require_relative "card"
+
 SUITS = [:hearts, :diamonds, :clubs, :spades]
 VALUES = ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"]
 
 class Deck
    
     def initialize 
-        @deck = []
-        #stack deck 
+      @deck = self.build_deck
     end
+    
+    def build_deck
+      deck = []
+      SUITS.each do |suit|
+        VALUES.each do |value|
+          deck << Card.new(value,suit)
+        end
+      end
+      deck
+    end
+
+
 end
--- w4d4/poker/spec/deck_spec.rb
+++ w4d4/poker/spec/deck_spec.rb
@@ -2,23 +2,43 @@ require "deck"
 
 describe "deck" do 
     deck = Deck.new
-    it "should define class constant available SUITS" do
+
+    it "should define class constant SUITS" do
         expect(SUITS.is_a?(Array)).to eq true
     end
 
-    it "should define class constant available VALUES" do
+    it "should define class constant VALUES" do
         expect(VALUES.is_a?(Array)).to eq true
     end
 
     describe "initialize" do 
-        it "should set deck equal to build_deck" do 
 
+        it "should set deck equal to #build_deck" do 
+          expect(deck.instance_variable_get(:@deck).length).to eq(52)
         end
 
-        it "should call ::build_deck" do 
+        # it "should call ::build_deck" do 
+        #   expect(self).to receive(:build_deck)
+        # end
 
-        end
+    end
 
-        it 
+    describe "build_deck" do 
+      it "should return full deck of 52 Card instances" do 
+        expect(deck.build_deck.length).to eq(52)
+        expect(deck.build_deck.all?{|card| card.is_a?(Card)}).to eq true
+      end
+
+      it "should return all "
+
+
+
+      it "should have all unique cards" do
+        built_deck = deck.build_deck 
+        expect(built_deck).to eq(built_deck.uniq)
+      end
+
+      
+      
     end
 end
