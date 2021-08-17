a5f3c8ecb242d5543ef54267d0532a454a147197
JoeJoegb922 <josephlam922@gmail.com>
2021-08-17 14:56:23 -0700

    rails views using html erb

---
+++ week_6/w6d5/ninety_nine_cats/app/controllers/cats_controller.rb
@@ -0,0 +1,44 @@
+class CatsController < ApplicationController
+
+    def index
+        @cats = Cat.all
+        render :index
+    end
+
+    def show
+        @cat = Cat.find_by(id: params[:id])
+        if @cat 
+            render :show
+        else
+            render json: { error: "There is no cat with that ID." }, status: 404
+        end
+    end
+
+    def new
+        @cat = Cat.new
+        render :new
+    end
+
+    def create
+        @cat = Cat.new(params.require(:cat).permit(:name, :color, :sex, :birth_date, :description))
+
+        if @cat.save
+            redirect_to cat_url(@cat)
+        else
+            render json: @cat.errors.full_messages, status: 422
+        end
+    end
+
+    def edit
+        @cat = Cat.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @cat = Cat.find_by(id: params[:id])
+        unless @cat 
+            render json: {error: "There is no such cat."}, status: 404
+        end
+    
+    end
+end
