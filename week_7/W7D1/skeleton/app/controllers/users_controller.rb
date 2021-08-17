84cb30b52b9088be81bf7dbafa5b2aff64573e32
JoeJoegb922 <josephlam922@gmail.com>
2021-08-17 14:54:56 -0700

    authorization of users

---
+++ week_7/W7D1/skeleton/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+    before_action :require_logged_in, only: [:index, :show]
+    
+    def index 
+        @users = User.all
+        render :index
+    end
+    
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login(@user)
+            redirect_to cats_url
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
+
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:user_name, :password)
+    end
+end
