395ca3f57258af117d63115e3610b13ddc53c637
JoeJoegb922 <josephlam922@gmail.com>
2021-08-20 09:42:19 -0700

    created user model and migration

---
+++ week_7/w7d5/reddit_clone/app/models/user.rb
@@ -0,0 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_session_token  (session_token) UNIQUE
+#  index_users_on_username       (username) UNIQUE
+#
+class User < ApplicationRecord
+    validates_presence_of :username, :session_token, :password_digest
+    validates_uniqueness_of :username, :session_token
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    attr_reader :password
+    after_initialize :ensure_session_token
+
+    #SPIRE
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.base64
+    end
+end
