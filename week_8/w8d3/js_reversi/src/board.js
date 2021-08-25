480a162b570d7226e97d5693464672c4c5838922
JoeJoegb922 <josephlam922@gmail.com>
2021-08-25 12:11:18 -0700

    finished piece and started board

--- week_8/w8d3/js_reversi/src/board.js
+++ week_8/w8d3/js_reversi/src/board.js
@@ -9,6 +9,16 @@ if (typeof window === 'undefined'){
  * and two white pieces at [3, 3] and [4, 4]
  */
 function _makeGrid () {
+  let grid = [];
+  for (let i = 0; i < 8; i++) {
+    grid.push(new Array(8));
+  }
+  grid[3][3] = new Piece("white")
+  grid[4][4] = new Piece("white")
+  grid[3][4] = new Piece("black")
+  grid[4][3] = new Piece("black")
+
+  return grid;
 }
 
 /**
@@ -28,6 +38,14 @@ Board.DIRS = [
  * Checks if a given position is on the Board.
  */
 Board.prototype.isValidPos = function (pos) {
+  if (pos[0] < 0 || pos[0] > 7) {
+    return false;
+  } else if (pos[1] < 0 || pos[1] > 7){
+    return false;
+  } else {
+    return true;
+  }
+
 };
 
 /**
@@ -35,6 +53,10 @@ Board.prototype.isValidPos = function (pos) {
  * throwing an Error if the position is invalid.
  */
 Board.prototype.getPiece = function (pos) {
+  let currentPos = this[pos[0], pos[1]]
+  if this.isValidPos(pos) {
+    return currentPos;
+  }
 };
 
 /**
