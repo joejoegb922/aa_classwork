55fa5682445cc8a31a48d2845aba54192c85bea0
Mike Le <letumichael@gmail.com>
2021-08-31 15:14:41 -0700

    Implemented event listener for clicks

--- week_9/w9d2/tictactoe_skeleton/src/ttt-view.js
+++ week_9/w9d2/tictactoe_skeleton/src/ttt-view.js
@@ -10,18 +10,39 @@ class View {
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         const li = document.createElement("li");
-        li.dataset.position = JSON.stringify([i,j])
+        li.dataset.position = JSON.stringify([i, j]);
         ul.append(li);
       }
     }
     this.el.append(ul);
   }
-  
-  bindEvents() {}
 
-  handleClick(e) {}
+  bindEvents() {
+    this.el.addEventListener("click", this.handleClick);
+  }
 
-  makeMove(square) {}
+  handleClick(e) { // e is a "synthetic event"
+    const se = e.target; // e.target refers to the DOM elements that triggered the event "e"
+    if ("li" === se.nodeName && this.makeMove(se)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  makeMove(square) {
+    // square is the position
+    const pos = JSON.parse(square.dataset.position);
+
+
+    if (pos === undefined) {
+      this.game.playMove(pos)
+      pos.classList.add(this.game.currentPlayer)
+    } else {
+          alert("Invalid move")
+    }
+
+  }
 
 }
 
